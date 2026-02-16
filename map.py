7cb77cfa6a7169a292e8f597d4a47b72c1c329a0
jcberk <jennifer.berk@donorschoose.org>
2026-02-16 14:05:37 -0500

    Schools with January hunger projects map v1

--- map.py
+++ map.py
@@ -1,3 +1,27 @@
-import streamlit as st
+# Imports
 
-st.write("map page - testing deploy")
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import os
+
+# Maintain state
+
+for key in st.session_state:
+    st.session_state[key] = st.session_state[key]
+
+# Get data
+
+@st.cache_data
+def get_january_schools():
+    url = os.environ["january_schools_secret"]
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    return conn.read(spreadsheet=url)
+
+january_schools = get_january_schools()
+
+# Display page
+
+st.header("Schools with Hunger Projects, January 2026")
+
+mappable_january_schools = january_schools.dropna(subset=["Latitude","Longitude"])
+st.map(data=mappable_january_schools, latitude="Latitude", longitude="Longitude")
